33c3d7b6628d4f2aab0cb77f7d15effb3dff9155
Raz <razielescamilla@outlook.com>
2020-04-28 01:31:54 -0500

    The cheapest permutation improvement

--- Competitive/EvaluateExpression.cpp
+++ Competitive/EvaluateExpression.cpp
@@ -5,9 +5,9 @@
 
 using namespace std;
 
-int32_t getValue(int8_t  Digits[7], int8_t DigitsPos ){
-    int32_t Value = 0;
-    int32_t Accum;
+int64_t getValue(int8_t  Digits[7], int8_t DigitsPos ){
+    int64_t Value = 0;
+    int64_t Accum;
     for(int d = 0; d < DigitsPos ; d++ ){
         Accum = pow(10,DigitsPos-d-1);
         Value += Digits[d] * Accum;
@@ -15,24 +15,40 @@ int32_t getValue(int8_t  Digits[7], int8_t DigitsPos ){
     return Value;
 }
 
-int32_t EvaluateExpression(string s){
-    int32_t Result = 0;
-    int32_t Value1;
-    int32_t Value2;
+int64_t EvaluateExpression(string s){
+    int64_t Result = 0;
+    int64_t Value1;
+    int64_t Value2;
     int8_t  Digits[7] = {0};
     int8_t  DigitsPos = 0;
-    int32_t op;
+    int64_t op;
     
     stack<char> s_Operators;
-    stack<int32_t> s_Values;
+    stack<int64_t> s_Values;
 
     for(int c = 0; c < s.length(); c++ ){
         if( s[c] == '+' || s[c] == '-'){
-
-            s_Operators.push(s[c]);
             Value1 = getValue( Digits, DigitsPos );
             s_Values.push(Value1);
             DigitsPos = 0;
+
+            if(!s_Operators.empty()){
+                Value2 = s_Values.top();
+                s_Values.pop();
+                Value1 = s_Values.top();
+                s_Values.pop();
+
+                if(s_Operators.top() == '+'){
+                    op = 1;
+                }
+                else if(s_Operators.top() == '-'){
+                    op = -1;
+                }
+                s_Operators.pop();
+                Result = Value1 + Value2 * op;
+                s_Values.push(Result);
+            }
+            s_Operators.push(s[c]);
             
         }
         else if ((s[c] >= '0') && (s[c] <='9')) {
@@ -63,12 +79,12 @@ int32_t EvaluateExpression(string s){
         Result = Value1 + Value2 * op;
         s_Values.push(Result);
     }
+    Result = s_Values.top();
+    s_Values.pop();
 
 
     return Result;
 }
-
-
 
 int main(void){
     string s;
--- Competitive/TheCheapestPermutation.cpp
+++ Competitive/TheCheapestPermutation.cpp
@@ -1,54 +1,43 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
 
 using namespace std;
 //The Cheapest Permutation problem
 
-int FindCheapestPermutation(int costs[9][9], vector<int> *a, int start, int  end){
+
+void FindCheapestPermutation2(int costs[9][9], vector<int> a, int level, vector<int>& min, int* minPermutationCost){
+    
     int permutationCost = 0;
-    int returnedCost;
-    if(start == end){
-        //compute cost
-        for(int i = 0; i < end; i++){
-            int row = a->at(i)-1;
-            int column = a->at(i+1)-1;
-            
+    int size = a.size();
+    if (size == level) 
+    { 
+        for(int i = 0; i < size - 1; i++){
+            int row = (a.at(i))-1;
+            int column = (a.at(i+1))-1;
             permutationCost += costs[row][column];
         }
-        //return cost
-        return permutationCost;
-    }
-
-    for(int j = start; j <= end; j++){
-        vector<int> a_aux = (*a);
-        //Swap value before recursive call...
-        int aux_int = a_aux[start];
-        a_aux[start] = a_aux[j];
-        a_aux[j] = aux_int;
-
-        //recursive call
-        returnedCost = FindCheapestPermutation(costs, &a_aux, start+1, end);
-        //compare returned cost
-        if (permutationCost == 0){
-            permutationCost = returnedCost;
+        if (permutationCost < *minPermutationCost){
+            *minPermutationCost = permutationCost;
+            min.assign(a.begin(), a.end());
         }
-        else if (returnedCost < permutationCost)
-        {
-            permutationCost = returnedCost;
-            a->assign(a_aux.begin(), a_aux.end());
-        }
-
+    } 
+    
+    for (int i=level; i<size; i++) 
+    { 
+        swap(a[level], a[i]);
+        FindCheapestPermutation2(costs,a,level + 1, min , minPermutationCost); 
+        swap(a[level], a[i]); 
     }
-    return permutationCost;
-}
-
-
+} 
 
 int main(void){
     int n;
     int Costs[9][9] = {0};
     int PermutationCost = 0;
+    int minPermutationCost = 65000;
     vector<int> a;
+    vector<int> m;
     
     cin >> n;
     //create original permutation with all values
@@ -61,10 +50,9 @@ int main(void){
             cin >> Costs[i][j];
         }
     }
-    PermutationCost = FindCheapestPermutation(Costs, &a, 0, n - 1);
+    FindCheapestPermutation2(Costs, a, 0, m, &minPermutationCost);
     for(int i = 0; i < n; i++){
-        cout << a[i] << " ";
+        cout << m[i] << " ";
     }
-    cout << endl;
 }
 
