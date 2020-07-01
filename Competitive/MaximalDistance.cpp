5d170ec11cb9fef40825e42fd5e659750888eef3
Raz <razielescamilla@outlook.com>
2020-07-01 02:22:12 -0500

    adding first solution to MaximalDistance problem

---
+++ Competitive/MaximalDistance.cpp
@@ -0,0 +1,66 @@
+//Maximal Distance
+
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+
+
+using namespace std;
+
+
+long int getDistance(long int a, long int b){
+    return abs(a - b);
+}
+
+void MaximalDistance(long int n){
+    long int MaxDistance;
+    long int xIndx;
+    long int x; 
+    long int yIndx;
+    long int y;
+
+    for(unsigned int i = 1; i <= n ; i ++){
+        long int aux;
+        cin >> aux;
+        if(i == 1){
+            //empty set, starting vales...
+            x = aux;
+            xIndx = i;
+            y = aux;
+            yIndx = i;
+            MaxDistance = 0;
+        }else{
+            //Compare new value and check if MaxDistance is greater than existing values 
+            long int max1 = getDistance(aux, x);
+            long int max2 = getDistance(aux, y);
+
+            if(max1 > MaxDistance || max2 > MaxDistance ){
+                //Replace the one with bigger distance
+                if(max1 > max2){
+                    //Replace Coordinate1
+                    y = aux;
+                    yIndx = i;
+                    MaxDistance = max1;
+                }else{
+                    //replace Coordinate2
+                    x = aux;
+                    xIndx = i;
+                    MaxDistance = max2;
+                }
+            }
+        }
+        cout << xIndx << " " << yIndx;
+    }
+
+}
+
+int main (void){
+
+    unsigned int n;
+    cin >> n;
+    MaximalDistance(n);
+    
+    return 0;
+}
+
+
