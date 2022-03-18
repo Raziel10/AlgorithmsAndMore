538f8a416b15fecea1487b09b198b9f227277da2
Raz <razielescamilla@outlook.com>
2022-03-17 23:14:01 -0600

    updating files

--- PowerSet.py
+++
@@ -1,8 +0,0 @@
-#Python program to find powerset 
-from itertools import combinations 
-def print_powerset(string): 
-    for i in range(0,len(string)+1): 
-        for element in combinations(string,i): 
-            print(''.join(element)) 
-string=['a','b','c'] 
-print_powerset(string) 
